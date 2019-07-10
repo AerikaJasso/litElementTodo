4f8ca9c6eddc466ab64652462292faa0cb7db574
Ærika <aerikajasso@gmail.com>
2019-07-09 17:35:46 -0700

    Create a class that extends litElement define render function and register the element

---
+++ src/views/todo-view.js
@@ -0,0 +1,11 @@
+import { LitElement, html } from "lit-element";
+
+class TodoView extends LitElement {
+  render() {
+    return html`
+    <p>Hello World</p>
+    `;
+  }
+}
+
+customElements.define('todo-view', TodoView);
